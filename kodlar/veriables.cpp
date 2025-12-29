3984cc20a029daf7608cdfcee2dd7cb5e7fe531e
emrullahkaratas <emrullahkaratas023@gmail.com>
2025-12-29 22:59:27 +0300

    Add files via upload

---
+++ kodlar/veriables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string isim ="Emrullah Karatas";
+    int yas = 21;
+    float Gano = 2.89f;
+    double pi = 3.1415926268383;
+    // bool true ve false değerlerini alır true ise 1 , false ise 0 
+    bool dogrumu =true; 
+    // ASCII tablosundan yardım alabilirsin yada tek harf ya da tek sayı girersin
+    char NotOrtalamasıHarf = 'B';
+
+    cout << "isim = " << isim << endl;
+    cout << "yaş = " << yas << endl;
+    cout << "Not ortalaması = " << Gano << endl;
+    cout << pi << endl;
+    cout << dogrumu << endl;
+    cout << "harf notu = " << NotOrtalamasıHarf << endl;
+}
